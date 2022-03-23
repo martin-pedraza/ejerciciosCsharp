c823af157043040658870846418fbd0b5fc50a15
martin-pedraza <56758686+martin-pedraza@users.noreply.github.com>
2022-03-23 19:58:49 -0300

    LaCalculadora

---
+++ La calculadora/LogicaNegocio/Calculadora.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LogicaNegocio
+{
+    public class Calculadora
+    {
+        public static int Calcular(int primerOperando, int segundoOperando, string operacionMatematica)
+        {
+            switch (operacionMatematica)
+            {
+                case "+":
+                    return primerOperando + segundoOperando;
+                case "-":
+                    return primerOperando - segundoOperando;
+                case "*":
+                    return primerOperando * segundoOperando;
+                case "/":
+                    if (Validar(segundoOperando))
+                    {
+                        return primerOperando / segundoOperando;
+                    }
+                    break;                    
+            }
+            return int.MinValue;
+        }
+        private static bool Validar(int segundoOperando)
+        {
+            return segundoOperando != 0;
+        }
+    }
+}
