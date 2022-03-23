1c52ee07764941c900a1b5ca38dbe4dda4289187
martin-pedraza <56758686+martin-pedraza@users.noreply.github.com>
2022-03-23 19:27:49 -0300

    ConversorBinario

---
+++ Conversor binario/LogicaNegocio/Conversor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace LogicaNegocio
+{
+    public class Conversor
+    {
+        public static string ConvertirDecimalABinario(int numeroEntero)
+        {
+            string binario = "";
+            string binarioOrdenado = "";
+            while (numeroEntero > 0)
+            {
+                if (numeroEntero % 2 == 0)
+                {
+                    binario += "0";
+                }
+                else
+                {
+                    binario += "1";
+                    numeroEntero--;
+                }
+                numeroEntero /= 2;
+            }
+            for (int i = 1; i <= binario.Length; i++)
+            {
+                binarioOrdenado += binario[^i];
+            }
+            return binarioOrdenado;
+        }
+        public static int ConvertirBinarioADecimal(int numeroEntero)
+        {
+            double numeroDecimal = 0;
+            for (int i = 0; i < numeroEntero.ToString().Length; i++)
+            {
+                if (numeroEntero.ToString()[i] == '1')
+                {
+                    numeroDecimal += Math.Pow(2, numeroEntero.ToString().Length - 1 - i);
+                }
+            }
+            return (int)numeroDecimal;
+        }
+    }
+}
