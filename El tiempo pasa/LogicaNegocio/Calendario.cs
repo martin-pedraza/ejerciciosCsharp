4b4f72bc43d86899ad48097d60fc82ce1468311d
martin-pedraza <56758686+martin-pedraza@users.noreply.github.com>
2022-03-25 19:52:37 -0300

    ElTiempoPasa

---
+++ El tiempo pasa/LogicaNegocio/Calendario.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LogicaNegocio
+{
+    public class Calendario
+    {
+        public static double CalcularDias(DateTime nacimiento)
+        {
+            return ((DateTime.Today - nacimiento).TotalHours) / 24;
+        }
+    }
+}
