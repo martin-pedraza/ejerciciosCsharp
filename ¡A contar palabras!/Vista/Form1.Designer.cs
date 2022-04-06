f717fffda942690c4adb8382cdfe48eebd83fec0
martin-pedraza <56758686+martin-pedraza@users.noreply.github.com>
2022-04-05 22:31:55 -0300

    AContarPalabras

---
+++ ¡A contar palabras!/Vista/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Vista
+{
+    partial class frmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rchParrafo = new System.Windows.Forms.RichTextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rchParrafo
+            // 
+            this.rchParrafo.Location = new System.Drawing.Point(12, 12);
+            this.rchParrafo.Name = "rchParrafo";
+            this.rchParrafo.Size = new System.Drawing.Size(384, 290);
+            this.rchParrafo.TabIndex = 0;
+            this.rchParrafo.Text = "";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(420, 354);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(119, 46);
+            this.btnCalcular.TabIndex = 1;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(551, 412);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.rchParrafo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Contador de Palabras";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rchParrafo;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
---
+++ ¡A contar palabras!/Vista/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vista
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            Dictionary<string, int> diccionario = new Dictionary<string, int>();
+            string[] palabra = rchParrafo.Text.Split(' ');
+            foreach (string item in palabra)
+            {
+                if (!diccionario.ContainsKey(item))
+                {
+                    diccionario.Add(item, 1);
+                }
+                else
+                {
+                    diccionario[item]++;
+                }
+            }    
+            mostrarRanking(diccionario);
+        }
+
+        public void mostrarRanking(Dictionary<string, int> diccionario)
+        {
+            StringBuilder ranking = new StringBuilder();
+            for (int i = 0; i < 3; i++)
+            {
+                foreach (KeyValuePair<string, int> item in diccionario)
+                {
+                    if (item.Value == diccionario.Values.Max())
+                    {
+                        ranking.AppendLine($"Palabra: {item.Key}, Cont: {item.Value}");
+                        diccionario.Remove(item.Key);
+                        break;
+                    }
+                }
+
+            }
+            MessageBox.Show(ranking.ToString());
+        }
+    }
+}
