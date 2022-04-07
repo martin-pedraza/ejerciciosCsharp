58c515f0b0bba53b16c2a12cd5084a7428c5d74c
martin-pedraza <56758686+martin-pedraza@users.noreply.github.com>
2022-04-06 23:06:50 -0300

    CreoQueNecesitoUnPrestamo

---
+++ Creo que necesito un préstamo/LogicaNegocio/Cuenta.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace LogicaNegocio
+{
+    public class Cuenta
+    {
+        private string titular;
+        private decimal cantidad;
+
+        public Cuenta(string titular, decimal cantidad)
+        {
+            this.titular = titular;
+            this.cantidad = cantidad;
+        }
+        public string GetTitular()
+        {
+            return this.titular;
+        }
+        public decimal GetCantidad()
+        {
+            return this.cantidad;
+        }
+        public string Mostrar()
+        {
+            return $"Titular: {this.titular}, Cantidad: {this.cantidad}";
+        }
+        public void Ingresar(decimal monto)
+        {
+            if (monto > 0)
+            {
+                this.cantidad += monto;
+            }
+        }
+        public void Retirar(decimal monto)
+        {
+            this.cantidad -= monto;
+        }
+    }
+}
