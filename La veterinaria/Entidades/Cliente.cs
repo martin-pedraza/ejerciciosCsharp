ed6657764eb0508e8c82df26a5560f050831a353
martin-pedraza <56758686+martin-pedraza@users.noreply.github.com>
2022-04-08 23:58:42 -0300

    LaVeterinaria

---
+++ La veterinaria/Entidades/Cliente.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Cliente
+    {
+        private string domicilio;
+        private string nombre;
+        private string apellido;
+        private string telefono;
+        private Mascota mascota1;
+        private Mascota mascota2;
+
+        public Cliente(string domicilio, string nombre, string apellido, string telefono, Mascota mascota1)
+        {
+            this.domicilio = domicilio;
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.telefono = telefono;
+            this.mascota1 = mascota1;
+        }
+        public void SetMascota2(Mascota mascota2)
+        {
+            this.mascota2 = mascota2;
+        }
+        public string GetDomicio()
+        {
+            return this.domicilio;
+        }
+        public string GetNombre()
+        {
+            return this.nombre;
+        }
+        public string GetApellido()
+        {
+            return this.apellido;
+        }
+        public string GetTelefono()
+        {
+            return this.telefono;
+        }
+        public string MostrarCliente()
+        {
+            StringBuilder text = new StringBuilder();
+            text.AppendLine("Cliente:");
+            text.AppendLine($"Nombre: {GetNombre()}");
+            text.AppendLine($"Apellido: {GetApellido()}");
+            text.AppendLine($"Domicilio: {GetDomicio()}");
+            text.AppendLine($"Telefono: {GetTelefono()}");
+            text.AppendLine(mascota1.MostrarMascota());
+            if (VerificarMascota2())
+            {
+                text.AppendLine(mascota2.MostrarMascota());
+            }
+            return text.ToString();
+        }
+        private bool VerificarMascota2()
+        {
+            if (this.mascota2 != null)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ La veterinaria/Entidades/Mascota.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text;
+
+namespace Entidades
+{
+    public class Mascota
+    {
+        private string especie;
+        private string nombre;
+        private DateTime fechaNacimiento;
+        private string historialVacunacion;
+
+        public Mascota(string especie, string nombre, DateTime fechaNacimiento)
+        {
+            this.especie = especie;
+            this.nombre = nombre;
+            this.fechaNacimiento = fechaNacimiento;
+        }
+        public void SetHistorialVacunacion(string historialVacunacion)
+        {
+            this.historialVacunacion = historialVacunacion;
+        }
+        public string GetEspecie()
+        {
+            return this.especie;
+        }
+        public string GetNombre()
+        {
+            return this.nombre;
+        }
+        public DateTime GetFechaNacimiento()
+        {
+            return this.fechaNacimiento;
+        }
+        public string GetHistorialVacunacion()
+        {
+            return this.historialVacunacion;
+        }
+        public string MostrarMascota()
+        {
+            StringBuilder text = new StringBuilder();
+            text.AppendLine("Mascota:");
+            text.AppendLine($"Nombre: {GetNombre()}");
+            text.AppendLine($"Especie: {GetEspecie()}");
+            text.AppendLine($"Fecha de Nacimiento: {GetFechaNacimiento().ToString("dd-MM-yyyy")}");
+            text.AppendLine($"Historial de vacunacion: {VerificarVacunacion()}");
+            return text.ToString();
+        }
+        private string VerificarVacunacion()
+        {
+            if (string.IsNullOrEmpty(GetHistorialVacunacion()))
+            {
+                return "Sin vacunar";
+            }
+            return GetHistorialVacunacion();
+        }
+    }
+}
