7526674448ee7afa86207b93a3434c10e48a3d72
martin-pedraza <56758686+martin-pedraza@users.noreply.github.com>
2022-04-10 14:53:16 -0300

    DenleUnaUIAEseConversorBinario

---
+++ Denle una UI a ese conversor binario/LogicaNegocio/NumeroBinario.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaNegocio
+{
+    public class NumeroBinario
+    {
+        private string numero;
+
+        private NumeroBinario(string numero)
+        {
+            this.numero = numero;
+        }
+        public string GetNumero()
+        {
+            return this.numero;
+        }
+        public static explicit operator NumeroBinario(NumeroDecimal numeroDecimal)
+        {
+            return new NumeroBinario(Conversor.ConvertirDecimalABinario(numeroDecimal.GetNumero()));
+        }
+        public static implicit operator NumeroBinario(string numero)
+        {
+            return new NumeroBinario(numero);
+        }
+        public static string operator +(NumeroBinario numeroBinario, NumeroDecimal numeroDecimal)
+        {
+            return Conversor.ConvertirDecimalABinario(((NumeroDecimal)numeroBinario).GetNumero() + numeroDecimal.GetNumero());
+        }
+        public static string operator -(NumeroBinario numeroBinario, NumeroDecimal numeroDecimal)
+        {
+            return Conversor.ConvertirDecimalABinario(((NumeroDecimal)numeroBinario).GetNumero() - numeroDecimal.GetNumero());
+        }
+        public static bool operator ==(NumeroBinario numeroBinario, NumeroDecimal numeroDecimal)
+        {
+            return numeroBinario.GetNumero() == ((NumeroBinario)numeroDecimal).GetNumero();
+        }
+        public static bool operator !=(NumeroBinario numeroBinario, NumeroDecimal numeroDecimal)
+        {
+            return !(numeroBinario==numeroDecimal);
+        }
+    }
+}
---
+++ Denle una UI a ese conversor binario/LogicaNegocio/NumeroDecimal.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaNegocio
+{
+    public class NumeroDecimal
+    {
+        private double numero;
+
+        private NumeroDecimal(double numero)
+        {
+            this.numero = numero;
+        }
+        public double GetNumero()
+        {
+            return this.numero;
+        }
+        public static explicit operator NumeroDecimal(NumeroBinario numeroBinario)
+        {
+            return new NumeroDecimal(Conversor.ConvertirBinarioADecimal(numeroBinario.GetNumero()));
+        }
+        public static implicit operator NumeroDecimal(double numero)
+        {
+            return new NumeroDecimal(numero);
+        }
+        public static double operator +(NumeroDecimal nD, NumeroBinario nb)
+        {
+            return nD.GetNumero() + Conversor.ConvertirBinarioADecimal(nb.GetNumero());
+        }
+        public static double operator -(NumeroDecimal nD, NumeroBinario nb)
+        {
+            return nD.GetNumero() - Conversor.ConvertirBinarioADecimal(nb.GetNumero());
+        }
+        public static bool operator ==(NumeroDecimal nD, NumeroBinario nb)
+        {
+            return nD.GetNumero() == Conversor.ConvertirBinarioADecimal(nb.GetNumero());
+        }
+        public static bool operator !=(NumeroDecimal nD, NumeroBinario nb)
+        {
+            return !(nD == nb);
+        }
+    }
+}
