e17c0b9cb37e395f32beeb2a707d22d74cbcc8f5
martin-pedraza <56758686+martin-pedraza@users.noreply.github.com>
2022-04-09 21:08:15 -0300

    ConversorBinarioRecargado

---
+++ Conversor binario recargado/LogicaNegocio/Conversor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace LogicaNegocio
+{
+    public class Conversor
+    {
+        public static string ConvertirDecimalABinario(double numeroEntero)
+        {
+            string binario = "";
+            string binarioOrdenado = "";
+            while (numeroEntero > 0)
+            {
+                if (numeroEntero % 2 == 0)
+                {
+                    binario += "0";
+                }
+                else
+                {
+                    binario += "1";
+                    numeroEntero--;
+                }
+                numeroEntero /= 2;
+            }
+            for (int i = 1; i <= binario.Length; i++)
+            {
+                binarioOrdenado += binario[^i];
+            }
+            return binarioOrdenado;
+        }
+        public static double ConvertirBinarioADecimal(string numeroEntero)
+        {
+            double numeroDecimal = 0;
+            for (int i = 0; i < numeroEntero.Length; i++)
+            {
+                if (numeroEntero[i] == '1')
+                {
+                    numeroDecimal += Math.Pow(2, numeroEntero.Length - 1 - i);
+                }
+            }
+            return numeroDecimal;
+        }
+    }
+}
