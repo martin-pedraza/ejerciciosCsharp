b86d2f6881c5d7cd511b610335bc8314747677b8
martin-pedraza <56758686+martin-pedraza@users.noreply.github.com>
2022-04-10 00:57:24 -0300

    Fahrenheit451

---
+++ Fahrenheit 451/Entidades/Conversor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Conversor
+    {
+        public static double ConvertirCelsiusFahrenheit(Celsius c)
+        {
+            return ((c.GetTemp() * (9 / 5)) + 32);
+        }
+        public static double ConvertirFahrenheitCelsius(Fahrenheit f)
+        {
+            return (f.GetTemp() - 32) * (5 / 9);
+        }
+        public static double ConvertirKelvinFahrenheir(Kelvin k)
+        {
+            return (k.GetTemp() * (9 / 5)) - 459.67;
+        }
+        public static double ConvertirFahrenheirKelvin(Fahrenheit f)
+        {
+            return (f.GetTemp() + 459.67) * (5 / 9);
+        }
+    }
+}
---
+++ Fahrenheit 451/Entidades/Fahrenheit.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Entidades
+{
+    public class Fahrenheit
+    {
+        private double temp;
+        public Fahrenheit(double temp)
+        {
+            this.temp = temp;
+        }
+        public double GetTemp()
+        {
+            return this.temp;
+        }
+        public static implicit operator Fahrenheit(double d)
+        {
+            return new Fahrenheit(d);
+        }
+        public static implicit operator double(Fahrenheit d)
+        {
+            return d.GetTemp();
+        }
+        public static explicit operator Celsius(Fahrenheit f)
+        {
+            return new Celsius(Conversor.ConvertirFahrenheitCelsius(f));
+        }
+        public static explicit operator Kelvin(Fahrenheit f)
+        {
+            return new Kelvin(Conversor.ConvertirFahrenheirKelvin(f));
+        }
+        public static double operator +(Fahrenheit f, Celsius c)
+        {
+            return f.GetTemp() + Conversor.ConvertirCelsiusFahrenheit(c);
+        }
+        public static double operator +(Fahrenheit f, Kelvin k)
+        {
+            return f.GetTemp() + Conversor.ConvertirKelvinFahrenheir(k);
+        }
+        public static double operator -(Fahrenheit f, Celsius c)
+        {
+            return f.GetTemp() - Conversor.ConvertirCelsiusFahrenheit(c);
+        }
+        public static double operator -(Fahrenheit f, Kelvin k)
+        {
+            return f.GetTemp() - Conversor.ConvertirKelvinFahrenheir(k);
+        }
+        public static bool operator ==(Fahrenheit f, Celsius c)
+        {
+            return f.GetTemp() == Conversor.ConvertirCelsiusFahrenheit(c);
+        }
+        public static bool operator ==(Fahrenheit f, Kelvin k)
+        {
+            return f.GetTemp() == Conversor.ConvertirKelvinFahrenheir(k);
+        }
+        public static bool operator !=(Fahrenheit f, Celsius c)
+        {
+            return !(f == c);
+        }
+        public static bool operator !=(Fahrenheit f, Kelvin k)
+        {
+            return !(f == k);
+        }
+    }
+}
