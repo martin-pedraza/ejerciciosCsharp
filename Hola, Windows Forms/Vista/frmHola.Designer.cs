568daf01e594296a55e6190b68871ecf81937859
martin-pedraza <56758686+martin-pedraza@users.noreply.github.com>
2022-04-10 12:28:28 -0300

    HolaWindowsForm

---
+++ Hola, Windows Forms/Vista/frmHola.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Vista
+{
+    partial class frmHola
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.btnSaludar = new System.Windows.Forms.Button();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.lblMateria = new System.Windows.Forms.Label();
+            this.cboMateria = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(10, 61);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 23);
+            this.txtNombre.TabIndex = 0;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(136, 61);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(100, 23);
+            this.txtApellido.TabIndex = 1;
+            // 
+            // btnSaludar
+            // 
+            this.btnSaludar.Location = new System.Drawing.Point(263, 61);
+            this.btnSaludar.Name = "btnSaludar";
+            this.btnSaludar.Size = new System.Drawing.Size(75, 23);
+            this.btnSaludar.TabIndex = 2;
+            this.btnSaludar.Text = "Saludar";
+            this.btnSaludar.UseVisualStyleBackColor = true;
+            this.btnSaludar.Click += new System.EventHandler(this.btnSaludar_Click);
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblNombre.Location = new System.Drawing.Point(10, 37);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(73, 21);
+            this.lblNombre.TabIndex = 3;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // lblApellido
+            // 
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblApellido.Location = new System.Drawing.Point(136, 37);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(75, 21);
+            this.lblApellido.TabIndex = 4;
+            this.lblApellido.Text = "Apellido";
+            // 
+            // lblMateria
+            // 
+            this.lblMateria.AutoSize = true;
+            this.lblMateria.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblMateria.Location = new System.Drawing.Point(10, 87);
+            this.lblMateria.Name = "lblMateria";
+            this.lblMateria.Size = new System.Drawing.Size(133, 21);
+            this.lblMateria.TabIndex = 5;
+            this.lblMateria.Text = "Materia favorita";
+            // 
+            // cboMateria
+            // 
+            this.cboMateria.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboMateria.FormattingEnabled = true;
+            this.cboMateria.Location = new System.Drawing.Point(10, 111);
+            this.cboMateria.Name = "cboMateria";
+            this.cboMateria.Size = new System.Drawing.Size(226, 23);
+            this.cboMateria.TabIndex = 6;
+            // 
+            // frmHola
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 162);
+            this.Controls.Add(this.cboMateria);
+            this.Controls.Add(this.lblMateria);
+            this.Controls.Add(this.lblApellido);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.btnSaludar);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtNombre);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmHola";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "¡Hola, Windows Forms!";
+            this.Load += new System.EventHandler(this.frmHola_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.Button btnSaludar;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblApellido;
+        private System.Windows.Forms.Label lblMateria;
+        private System.Windows.Forms.ComboBox cboMateria;
+    }
+}
---
+++ Hola, Windows Forms/Vista/frmHola.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vista
+{
+    public partial class frmHola : Form
+    {
+        public frmHola()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSaludar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(this.txtNombre.Text) && string.IsNullOrWhiteSpace(this.txtApellido.Text))
+            {
+                MessageBox.Show("Se debe completar nombre y apellido.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (string.IsNullOrWhiteSpace(this.txtNombre.Text) && !string.IsNullOrWhiteSpace(this.txtApellido.Text))
+            {
+                MessageBox.Show("Se debe completar nombre.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (!string.IsNullOrWhiteSpace(this.txtNombre.Text) && string.IsNullOrWhiteSpace(this.txtApellido.Text))
+            {
+                MessageBox.Show("Se debe completar apellido.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                frmSaludo frmSaludo = new frmSaludo("¡Hola, Windows Forms!", $"Soy {this.txtNombre.Text} {this.txtApellido.Text} y mi materia favorita es {this.cboMateria.SelectedItem.ToString()}");
+                frmSaludo.ShowDialog();
+            }
+        }
+
+        private void frmHola_Load(object sender, EventArgs e)
+        {
+            this.cboMateria.Items.Add("Programacion I");
+            this.cboMateria.Items.Add("Laboratorio I");
+            this.cboMateria.Items.Add("Ingles I");
+            this.cboMateria.Items.Add("Sistema operativo");
+            this.cboMateria.Items.Add("Matematicas");
+            this.cboMateria.Items.Add("Programacion II");
+            this.cboMateria.Items.Add("Laboratorio II");
+            this.cboMateria.Items.Add("Investigacion Operativa");
+            this.cboMateria.Items.Add("Estadistica");
+            this.cboMateria.Items.Add("Ingles II");
+            this.cboMateria.SelectedIndex = 0;
+        }
+    }
+}
