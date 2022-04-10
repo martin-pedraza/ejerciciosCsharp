5a1204649d9c93d56cf4f58ab7b73afcde9fc688
martin-pedraza <56758686+martin-pedraza@users.noreply.github.com>
2022-04-10 14:26:53 -0300

    Registrate

---
+++ Registrate/Entidades/Ingresante.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+
+namespace Entidades
+{
+    public class Ingresante
+    {
+        private string[] cursos;
+        private string direccion;
+        private int edad;
+        private string genero;
+        private string nombre;
+        private string pais;
+
+        public Ingresante(string[] cursos, string direccion, int edad, string genero, string nombre, string pais)
+        {
+            this.cursos = cursos;
+            this.direccion = direccion;
+            this.edad = edad;
+            this.genero = genero;
+            this.nombre = nombre;
+            this.pais = pais;
+        }
+        public string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Nombre: {this.nombre}");
+            sb.AppendLine($"Direccion: {this.direccion}");
+            sb.AppendLine($"Edad: {this.edad}");
+            sb.AppendLine($"Genero: {this.genero}");
+            sb.AppendLine($"Pais: {this.pais}");
+            sb.AppendLine("Cursos: ");
+            foreach (string s in this.cursos)
+            {
+                if (s is not null)
+                {
+                    sb.AppendLine(s);
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
