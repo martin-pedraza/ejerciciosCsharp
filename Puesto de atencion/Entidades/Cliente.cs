8b3a91fca9233b979d80b80943bc83537e8f30a5
martin-pedraza <56758686+martin-pedraza@users.noreply.github.com>
2022-04-11 22:08:55 -0300

    PuestoDeAtencion

---
+++ Puesto de atencion/Entidades/Cliente.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Entidades
+{
+    public class Cliente
+    {
+        private string nombre;
+        private int numero;
+
+        public string Nombre
+        {
+            get { return this.nombre; }
+            set { this.nombre = value; }
+        }
+        public int Numero
+        {
+            get { return this.numero; }
+        }
+        public Cliente(int numero)
+        {
+            this.numero = numero;
+        }
+        public Cliente(int numero, string nombre) : this(numero)
+        {
+            this.nombre = nombre;
+        }
+        public static bool operator ==(Cliente c1, Cliente c2)
+        {
+            return c1.Nombre == c2.Nombre;
+        }
+        public static bool operator !=(Cliente c1, Cliente c2)
+        {
+            return !(c1 == c2);
+        }
+    }
+}
---
+++ Puesto de atencion/Entidades/Negocio.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Negocio
+    {
+        private PuestoAtencion caja;
+        private Queue<Cliente> clientes;
+        private string nombre;
+
+        private Negocio()
+        {
+            this.caja = new PuestoAtencion(PuestoAtencion.Puesto.Caja1);
+            this.clientes = new Queue<Cliente>();
+        }
+        public Negocio(string nombre) :this()
+        {
+            this.nombre = nombre;
+        }
+        public static bool operator +(Negocio n, Cliente c)
+        {
+            foreach (Cliente item in n.clientes)
+            {
+                if (item== c)
+                {
+                    return false;
+                }
+            }
+            n.clientes.Enqueue(c);
+            return true;
+        }
+        public Cliente Cliente
+        {
+            get{ return this.clientes.Dequeue(); }
+            set{ bool resultado = this + value; }
+        }
+        public static bool operator ==(Negocio n, Cliente c)
+        {
+            foreach (Cliente item in n.clientes)
+            {
+                if (item == c)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static bool operator !=(Negocio n, Cliente c)
+        {
+            return !(n == c);
+        }
+        public int ClientesPendientes
+        {
+            get { return this.clientes.Count; }
+        }
+        public static bool operator ~(Negocio n)
+        {
+            if (n.clientes.Count > 0)
+            {
+                return n.caja.Atender(n.Cliente);
+            }
+            return false;
+        }
+    }
+}
