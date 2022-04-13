5744201b431df63014651ad8e2f87297fff47d66
martin-pedraza <56758686+martin-pedraza@users.noreply.github.com>
2022-04-12 22:35:53 -0300

    EstadisticaDeportiva

---
+++ Estadistica deportiva/Entidades/Equipo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Equipo
+    {
+        private short cantidadDeJugadores;
+        private List<Jugador> jugadores;
+        private string nombre;
+
+        private Equipo()
+        {
+            this.jugadores = new List<Jugador>(this.cantidadDeJugadores);
+        }
+        public Equipo(short cantidadDeJugadores, string nombre) :this()
+        {
+            this.cantidadDeJugadores = cantidadDeJugadores;
+            this.nombre = nombre;
+        }
+        public static bool operator +(Equipo e, Jugador j)
+        {
+            foreach (Jugador item in e.jugadores)
+            {
+                if (item == j)
+                {
+                    return false;
+                }
+            }
+            e.jugadores.Add(j);
+            return true;
+        }
+    }
+}
---
+++ Estadistica deportiva/Entidades/Jugador.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Entidades
+{
+    public class Jugador
+    {
+        private int dni;
+        private string nombre;
+        private int partidosJugados;
+        private float promedioGoles;
+        private int totalGoles;
+
+        public float PromedioGoles
+        {
+            get { return (float)this.totalGoles / this.partidosJugados; }
+        }
+        private Jugador()
+        {
+            this.partidosJugados = 0;
+            this.promedioGoles = 0;
+            this.totalGoles = 0;
+        }
+
+        public Jugador(int dni, string nombre) : this()
+        {
+            this.dni = dni;
+            this.nombre = nombre;
+        }
+
+        public Jugador(int dni, string nombre, int partidosJugados, int totalGoles)
+        {
+            this.dni = dni;
+            this.nombre = nombre;
+            this.partidosJugados = partidosJugados;
+            this.totalGoles = totalGoles;
+        }
+        public string MostrarDatos()
+        {
+            return $"Nombre: {this.nombre}, DNI: {this.dni}, Partidos: {this.partidosJugados}, Goles: {this.totalGoles}, Promedio de gol: {this.PromedioGoles}";
+        }
+        public static bool operator ==(Jugador j1, Jugador j2)
+        {
+            return j1.dni == j2.dni;
+        }
+        public static bool operator !=(Jugador j1, Jugador j2)
+        {
+            return !(j1 == j2);
+        }
+    }
+}
