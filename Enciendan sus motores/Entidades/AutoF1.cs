5ccaa20be38ef660a2d7e794ca5e0f6d8645012a
martin-pedraza <56758686+martin-pedraza@users.noreply.github.com>
2022-04-12 23:28:44 -0300

    EnciendasSusMotores

---
+++ Enciendan sus motores/Entidades/AutoF1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Entidades
+{
+    public class AutoF1
+    {
+        private short cantidadCombustible;
+        private bool enCompetencia;
+        private string escuderia;
+        private short numero;
+        private short vueltasRestantes;
+
+        public AutoF1(short numero, string escuderia)
+        {
+            this.numero = numero;
+            this.escuderia = escuderia;
+            this.enCompetencia = false;
+            this.cantidadCombustible = 0;
+            this.vueltasRestantes = 0;
+        }
+        public short CantidadCombustible
+        {
+            get { return this.cantidadCombustible; }
+            set { this.cantidadCombustible = value; }
+        }
+        public short VueltasRestantes
+        {
+            get { return this.vueltasRestantes; }
+            set { this.vueltasRestantes = value; }
+        }
+        public bool EnCompetencia
+        {
+            get { return this.enCompetencia; }
+            set { this.enCompetencia = value; }
+        }
+        public static bool operator ==(AutoF1 a1, AutoF1 a2)
+        {
+            return a1.numero == a2.numero && a1.escuderia == a2.escuderia;
+        }
+        public static bool operator !=(AutoF1 a1, AutoF1 a2)
+        {
+            return !(a1 == a2);
+        }
+        public string MostrarDato()
+        {
+            return $"Numero: {this.numero}, Escuderia: {this.escuderia}, Combustible: {this.CantidadCombustible}, Vueltas: {this.VueltasRestantes}, En competencia: {this.EnCompetencia}";
+        }
+    }
+}
---
+++ Enciendan sus motores/Entidades/Competencia.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Competencia
+    {
+        private short cantidadCompetidores;
+        private short cantidadVueltas;
+        private List<AutoF1> competidores;
+
+        private Competencia()
+        {
+            this.competidores = new List<AutoF1>(this.cantidadCompetidores);
+        }
+        public Competencia(short cantidadVueltas, short cantidadCompetidores) :this()
+        {
+            this.cantidadCompetidores = cantidadCompetidores;
+            this.cantidadVueltas = cantidadVueltas;
+        }
+        public static bool operator +(Competencia c, AutoF1 a)
+        {
+            if (c.competidores.Count() < c.cantidadCompetidores && c != a)
+            {
+                c.competidores.Add(a);
+                a.EnCompetencia = true;
+                a.VueltasRestantes = c.cantidadVueltas;
+                a.CantidadCombustible = (short)new Random().Next(15, 100);
+                return true;
+            }
+            return false;
+        }
+        public static bool operator -(Competencia c, AutoF1 a)
+        {
+            if (c == a)
+            {
+                c.competidores.Remove(a);
+                return true;
+            }
+            return false;
+        }
+        public static bool operator ==(Competencia c, AutoF1 a)
+        {
+            foreach (AutoF1 item in c.competidores)
+            {
+                if (item == a)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static bool operator !=(Competencia c, AutoF1 a)
+        {
+            return !(c == a);
+        }
+        public string MostrarDatos()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Vueltas: {this.cantidadVueltas}, Competidores: {this.cantidadCompetidores}");
+            foreach (AutoF1 item in this.competidores)
+            {
+                sb.AppendLine(item.MostrarDato());
+            }
+            return sb.ToString();
+        }
+    }
+}
