357920a5e6bdae71521f1a7ed1da62114eba367e
martin-pedraza <56758686+martin-pedraza@users.noreply.github.com>
2022-04-26 14:33:54 -0300

    LaCentralitaEpisodioII

---
+++ La centralita Episodio II/Centralita/Centralita.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Centralita
+    {
+        private List<Llamada> listaDeLlamadas;
+        protected string razonSocial;
+
+        public List<Llamada> Llamadas
+        {
+            get { return this.listaDeLlamadas; }
+        }
+        public float GananciasPorLocal
+        {
+            get { return CalcularGanancia(TipoLLamada.Local); }
+        }
+        public float GananciasPorProvincial
+        {
+            get { return CalcularGanancia(TipoLLamada.Provincial); }
+        }
+        public float GananciasPorTotal
+        {
+            get { return CalcularGanancia(TipoLLamada.Todas); }
+        }
+        public Centralita()
+        {
+            this.listaDeLlamadas = new List<Llamada>();
+        }
+        public Centralita(string nombreEmpresa) : this()
+        {
+            this.razonSocial = nombreEmpresa;
+        }
+        private float CalcularGanancia(TipoLLamada tipo)
+        {
+            float sumadorLocal = 0;
+            float sumadorProvincial = 0;
+            foreach (var item in this.Llamadas)
+            {
+                if (item is Local)
+                {
+                    sumadorLocal += ((Local)item).CostoLlamada;
+                }
+                if (item is Provincial)
+                {
+                    sumadorProvincial += ((Provincial)item).CostoLlamada;
+                }
+            }
+            switch (tipo)
+            {
+                case TipoLLamada.Local:
+                    return sumadorLocal;
+                case TipoLLamada.Provincial:
+                    return sumadorProvincial;
+                default:
+                    return sumadorLocal + sumadorProvincial;
+            }
+        }
+        private string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Razon social: {this.razonSocial}\n"
+                + $"Ganancias ({this.GananciasPorTotal}):\n"
+                + $"Local: {this.GananciasPorLocal}\n"
+                + $"Provincial: {this.GananciasPorProvincial}\n"
+                + "--------------------\n"
+                + "Llamadas:");
+            foreach (var item in this.Llamadas)
+            {
+                sb.AppendLine(item.ToString());
+            }
+            return sb.ToString();
+        }
+        public void OrdenarLlamadas()
+        {
+            this.listaDeLlamadas.Sort(Llamada.OrdenarPorDuracion);
+        }
+        public override string ToString()
+        {
+            return this.Mostrar();
+        }
+        private void AgregarLlamada(Llamada nuevaLlamada)
+        {
+            this.listaDeLlamadas.Add(nuevaLlamada);
+        }
+        public static bool operator ==(Centralita c, Llamada llamada)
+        {
+            foreach (Llamada item in c.listaDeLlamadas)
+            {
+                if (item == llamada)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static bool operator !=(Centralita c, Llamada llamada)
+        {
+            return !(c == llamada);
+        }
+        public static bool operator +(Centralita c, Llamada nuevaLlamada)
+        {
+            if (c != nuevaLlamada)
+            {
+                c.AgregarLlamada(nuevaLlamada);
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ La centralita Episodio II/Centralita/Llamada.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+using Entidades;
+
+namespace Entidades
+{
+    public enum TipoLLamada { Local, Provincial, Todas };
+    public abstract class Llamada
+    {
+        protected float duracion;
+        protected string nroDestino;
+        protected string nroOrigen;
+        public float Duracion
+        {
+            get { return this.duracion; }
+        }
+        public string NroOrigen
+        {
+            get { return this.nroOrigen; }
+        }
+        public string NroDestino
+        {
+            get { return this.nroDestino; }
+        }
+        public abstract float CostoLlamada { get; }
+        public Llamada(float duracion, string nroDestino, string nroOrigen)
+        {
+            this.duracion = duracion;
+            this.nroDestino = nroDestino;
+            this.nroOrigen = nroOrigen;
+        }
+        protected virtual string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Duracion: {this.Duracion.ToString()}");
+            sb.AppendLine($"Origen: {this.NroOrigen}");
+            sb.AppendLine($"Destino: {this.NroDestino}");
+            return sb.ToString();
+        }
+        public static bool operator ==(Llamada l1, Llamada l2)
+        {
+            return l1.GetType() == l2.GetType() && l1.NroDestino == l2.NroDestino && l1.NroOrigen == l2.NroOrigen;
+        }
+        public static bool operator !=(Llamada l1, Llamada l2)
+        {
+            return !(l1 == l2);
+        }
+        public static int OrdenarPorDuracion(Llamada llamada1, Llamada llamada2)
+        {
+            return (int)llamada2.Duracion - (int)llamada1.Duracion;
+        }
+    }
+}
