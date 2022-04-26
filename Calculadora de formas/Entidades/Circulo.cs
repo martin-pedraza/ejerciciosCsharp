03ef62854b16c54dc6fe34499cb777d120ab9d4f
martin-pedraza <56758686+martin-pedraza@users.noreply.github.com>
2022-04-26 10:34:56 -0300

    CalculadoraDeFormas

---
+++ Calculadora de formas/Entidades/Circulo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Circulo : Figura
+    {
+        private double radio;
+        public Circulo(double radio)
+        {
+            this.Radio = radio;
+        }
+        public double Radio
+        {
+            get { return this.radio; }
+            set { this.radio = value; }
+        }
+        public override string Dibujar()
+        {
+            return "Dibujando círculo...";
+        }
+        public override double CalcularSuperficie()
+        {
+            return Math.PI * Math.Pow(this.Radio, 2);
+        }
+        public override double CalcularPerimetro()
+        {
+            return Math.PI * (2 * this.Radio);
+        }
+    }
+}
---
+++ Calculadora de formas/Entidades/Cuadrado.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Cuadrado : Rectangulo
+    {
+        private double lado;
+        public Cuadrado(double lado) :base(lado, lado) { }
+        public double Lado
+        {
+            get { return this.lado; }
+            set { this.lado = value; }
+        }
+        public override string Dibujar()
+        {
+            return "Dibujando cuadrado...";
+        }
+    }
+}
