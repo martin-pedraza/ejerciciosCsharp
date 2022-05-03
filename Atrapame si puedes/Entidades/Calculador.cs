17b8743f68838ee22313ec910583249839204101
martin-pedraza <56758686+martin-pedraza@users.noreply.github.com>
2022-05-02 22:54:39 -0300

    AtrapameSiPuedes

---
+++ Atrapame si puedes/Entidades/Calculador.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public static class Calculador
+    {
+        public static decimal Calcular(int kilometro, int litro)
+        {
+            try
+            {
+                return (decimal)kilometro / litro;
+            }
+            catch (DivideByZeroException)
+            {
+                throw;
+            }
+        }
+    }
+}
