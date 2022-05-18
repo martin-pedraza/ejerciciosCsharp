15bd82db10af49a3049dfc0948e3a460eb0d5153
martin-pedraza <56758686+martin-pedraza@users.noreply.github.com>
2022-05-18 19:50:59 -0300

    ContadorDeDigitos

---
+++ Contador de dígitos/Entidades/ExtensionInt64.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Entidades
+{
+    public static class ExtensionInt64
+    {
+        public static int ContarCantidadDeDigitos(this Int64 i)
+        {
+            int contador = 0;
+            while (i > 0)
+            {
+                contador++;
+                i /= 10;
+                
+            } 
+            return contador;
+        }
+    }
+}
---
+++ Contador de dígitos/Vista/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Entidades;
+
+namespace Vista
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Int64 n;
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Ingrese un numero:\t");
+                n = Int64.Parse(Console.ReadLine());
+                Console.WriteLine($"Numero de \t\t{n.ContarCantidadDeDigitos()} digitos");
+            }
+        }
+    }
+}
