69dcd32026417be9f64e78f325cd5303dff36c35
martin-pedraza <56758686+martin-pedraza@users.noreply.github.com>
2022-05-18 19:59:12 -0300

    PuntoYSeguido

---
+++ Punto y seguido/Entidades/ExtensionString.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Entidades
+{
+    public static class ExtensionString
+    {
+        public static int ContarSignosDePuntuacion(this String s)
+        {
+            int contador = 0;
+            foreach (char c in s)
+            {
+                if (c == '.' || c == ',' || c == ';')
+                {
+                    contador++;
+                }
+            }
+            return contador;
+        }
+    }
+}
