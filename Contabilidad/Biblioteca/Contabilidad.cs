dee3f6082aa1d0b3c419893ff10bcc6cd3c94296
martin-pedraza <56758686+martin-pedraza@users.noreply.github.com>
2022-05-25 17:13:38 -0300

    Contabilidad

---
+++ Contabilidad/Biblioteca/Contabilidad.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class Contabilidad<T, U> where T : Documento
+        where U : Documento, new()
+    {
+        private List<T> egresos;
+        private List<U> ingresos;
+
+        public Contabilidad()
+        {
+            this.egresos = new List<T>();
+            this.ingresos = new List<U>();
+        }
+        public string Egresos
+        {
+            get
+            {
+                StringBuilder sb = new StringBuilder();
+                foreach (T t in this.egresos)
+                {
+                    sb.AppendLine($"Factura: {t.ToString()}");
+                }
+                return sb.ToString();
+            }
+        }
+        public string Ingresos
+        {
+            get
+            {
+                StringBuilder sb = new StringBuilder();
+                foreach (U u in this.ingresos)
+                {
+                    sb.AppendLine($"Recibo: {u.ToString()}");
+                }
+                return sb.ToString();
+            }
+        }
+        public static Contabilidad<T, U> operator +(Contabilidad<T, U> c, T egreso)
+        {
+            c.egresos.Add(egreso);
+            return c;
+        }
+        public static Contabilidad<T, U> operator +(Contabilidad<T, U> c, U ingreso)
+        {
+            c.ingresos.Add(ingreso);
+            return c;
+        }
+    }
+}
---
+++ Contabilidad/Consola/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Biblioteca;
+
+namespace Consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Recibo recibo1 = new Recibo();
+            Recibo recibo2 = new Recibo(6);
+            Recibo recibo3 = new Recibo(10);
+            Factura factura1 = new Factura(555);
+            Factura factura2 = new Factura(100);
+            Factura factura3 = new Factura(156);
+            Contabilidad<Factura, Recibo> contabilidad = new Contabilidad<Factura, Recibo>();
+
+            contabilidad += recibo1;
+            contabilidad += recibo2;
+            contabilidad += recibo3;
+
+            contabilidad += factura1;
+            contabilidad += factura2;
+            contabilidad += factura3;
+
+            Console.WriteLine(contabilidad.Egresos);
+            Console.WriteLine(contabilidad.Ingresos);
+        }
+    }
+}
